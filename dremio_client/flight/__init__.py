566e3fa5d42b81b26d0c99c1329d0e99eeab7031
Ambrose Guo <guohongyi1991@gmail.com>
2021-11-24 17:52:06 +0800

    flight remove low cost queue

--- dremio_client/flight/__init__.py
+++ dremio_client/flight/__init__.py
@@ -106,9 +106,7 @@ try:
         if username and password:
             encoded_credentials = base64.b64encode(b'' + username.encode() + b':' + password.encode())
             initial_options = flight.FlightCallOptions(headers=[
-                (b'authorization', b'Basic ' + encoded_credentials),
-                (b'routing-tag', b'test-routing-tag'),
-                (b'routing-queue', b'Low Cost User Queries')
+                (b'authorization', b'Basic ' + encoded_credentials)
             ])
             # token_pair = client.authenticate_basic_token(username, password, initial_options)
             # client.authenticate(HttpDremioClientAuthHandler(username, password if password else ""), initial_options)
