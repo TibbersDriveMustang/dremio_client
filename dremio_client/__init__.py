a23e6fd4273fe7303979f89100d0b2dc31bba9d9
Ambrose Guo <guohongyi1991@gmail.com>
2021-11-26 14:25:09 +0800

    0.15.1a

--- dremio_client/__init__.py
+++ dremio_client/__init__.py
@@ -34,7 +34,7 @@ from .flight.flight_auth import DEFAULT_SSL_ROOT_CERT
 
 __author__ = """Ryan Murray"""
 __email__ = "rymurr@gmail.com"
-__version__ = "0.16.0"
+__version__ = "0.15.1a"
 
 
 def get_config(config_dir=None, args=None):
